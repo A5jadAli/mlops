e5acd2dacd82bcec5c8ea1636cde627a346e6d60
Asjad <aliasjid009@gmail.com>
2024-08-23 19:49:14 +0500

    first commit before dvc

---
+++ dvc-for-mlops/mycode.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+
+# create a sample dataframe with names, ages and cities
+data = {
+    'Name': ['Tom', 'Nick', 'John', 'Smith', 'Sam'],
+    'Age': [20, 21, 19, 18, 22],
+    'City': ['New York', 'California', 'Texas', 'Florida', 'Washington']
+}
+
+df = pd.DataFrame(data)
+
+data_dir = 'data' # directory to save the data
+os.makedirs(data_dir, exist_ok=True) # create the directory if it doesn't exist
+
+# define the file path to save the data
+file_path = os.path.join(data_dir, 'sample_data.csv')  # data/sample_data.csv
+
+# save the dataframe to a csv file
+df.to_csv(file_path, index=False)
+
+print(f"Data saved to {file_path}")
